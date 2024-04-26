bcd3d49498343d08f1e075e830c756680757d3cf
muslirou <murilo.f.domingues@gmail.com>
2024-04-26 15:51:45 -0300

    Inicio do Projeto Spring

---
+++ src/main/java/murilo/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,27 @@
+package murilo.screenmatch;
+
+import murilo.screenmatch.model.DadosSerie;
+import murilo.screenmatch.service.ConsumoAPI;
+import murilo.screenmatch.service.ConverteDados;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoApi = new ConsumoAPI();
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=ugly+betty&apikey=c2a9a407");
+		System.out.println(json);
+
+		var conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
+	}
+}
